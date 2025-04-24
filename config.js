d3407414f2be9015b6f3d0736595a31480cea17a
Tien Mai Van <83681017+tienmaivan02@users.noreply.github.com>
2025-04-24 23:19:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,3 +17,8 @@ const CONFIG = {
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
+//sdaiflksmdlnsabfuwlnalnflnadslfnasd: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+   // btnYes: 'Vẫn cứ là thích anh <33',
+//    btnNo: 'Không, Anh trai à :3',
+ //   question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+  //  btnReply: 'Gửi cho anh <3',
